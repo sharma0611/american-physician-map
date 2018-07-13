989559ddd0b6e4d1d52b43a9b97dd1a09f02bf0d
sharma0611 <sharma.shivam0611@gmail.com>
2018-07-13 12:40:04 -0400

    start django project skeleton

---
+++ physician_maps/physician_maps/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for physician_maps project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "physician_maps.settings")
+
+application = get_wsgi_application()
