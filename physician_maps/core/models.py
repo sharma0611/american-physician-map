003bc133ebc3a06c71207aaf2426d2dfe8d254cd
sharma0611 <sharma.shivam0611@gmail.com>
2018-07-14 16:33:04 -0400

    create doctor model

--- physician_maps/core/models.py
+++ physician_maps/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField("Name", max_length=200)
+    address = models.CharField("Address", max_length=200) #, unique=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True, default=None)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True,null=True, default=None)
+
+
