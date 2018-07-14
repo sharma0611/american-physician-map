e5dba588dc9235819e56523a041b50be988f840c
sharma0611 <sharma.shivam0611@gmail.com>
2018-07-14 16:37:14 -0400

    registered doctor model in admin panel

--- physician_maps/core/admin.py
+++ physician_maps/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Doctor
+
+# Register your models here.
+admin.site.register(Doctor)
+admin.site.site_header = 'American Physicians'
+
