b87bb5837ebed5eaedeee2cf57c34f87e09c61d8
sharma0611 <sharma.shivam0611@gmail.com>
2018-07-16 20:08:22 -0400

    add coordinate retreival function

--- physician_maps/core/models.py
+++ physician_maps/core/models.py
@@ -1,11 +1,37 @@
 from django.db import models
+from django.conf import settings
+gmaps = googlemaps.Client(key=settings.GMAPS_KEY)
+
+# Model helper functions
+
+def get_latitude_longitude(address, zipcode):
+    geocode_result = gmaps.geocode(address, components={'postal_code':zipcode, 'country:':"US"})
+    lat = geocode_result[0]['geometry']['location']['lat']
+    lng = geocode_result[0]['geometry']['location']['lng']
+    return lat, lng
 
 # Create your models here.
 
 class Doctor(models.Model):
     name = models.CharField("Name", max_length=200)
     address = models.CharField("Address", max_length=200) #, unique=True)
+    zipcode = models.CharField("Zipcode", max_length=20)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True, default=None)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True,null=True, default=None)
+
+    def get_coordinates(self):
+        lat = self.latitude
+        lng = self.longitude
+        if lat and lng:
+            return (lat, lng)
+        else:
+            try:
+                lat, lng = get_latitude_longitude(self.address)
+            except:
+                lat, lng = (None, None)
+            self.latitude = lat
+            self.longitude = lng
+            self.save()
+            return lat, lng
 
 
