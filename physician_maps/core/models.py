41cdb457a9a470f12a02e123d0e7db966c1d8430
sharma0611 <sharma.shivam0611@gmail.com>
2018-07-17 00:57:10 -0400

    update lat long request func

--- physician_maps/core/models.py
+++ physician_maps/core/models.py
@@ -6,13 +6,12 @@ gmaps = googlemaps.Client(key=settings.GMAPS_KEY)
 # Model helper functions
 
 def get_latitude_longitude(address, zipcode):
-    geocode_result = gmaps.geocode(address)
+    geocode_result = gmaps.geocode(address, components={'postal_code': zipcode, 'country': 'US'})
     lat = geocode_result[0]['geometry']['location']['lat']
     lng = geocode_result[0]['geometry']['location']['lng']
     return lat, lng
 
 # Create your models here.
-
 class Doctor(models.Model):
     name = models.CharField("Name", max_length=200)
     address = models.CharField("Address", max_length=200) #, unique=True)
