f8139a00d9e4a5b46179d981a423b55903ce7f00
sharma0611 <sharma.shivam0611@gmail.com>
2018-07-18 12:56:49 -0400

    upgraded autocomplete with ui bug fixes

--- physician_maps/core/static/js/index.js
+++ physician_maps/core/static/js/index.js
@@ -1,61 +1,85 @@
-var SearchControl = document.getElementById('SBox');
-var ColorChange = document.getElementById('color');
-
-$("#searchclick").click(function(event) {
-// do the extra stuff her
-event.preventDefault();
-console.log( "Handler for .submit() called." );
-var query = $(document.getElementById('search')).val();
-
-$.get("doctorsearch", {'query': query})
-  .done(function(data){
-	    var latlng = new google.maps.LatLng(data.latitude, data.longitude);
-	    if (activeInfoWindow) { activeInfoWindow.close();}
-	    marker.setPosition(latlng);
-	    map.panTo(latlng);
-	    map.setZoom(13);
-	    var contentString = '<div class="info-window"> <h2>' + data.title + '</h2>';
-	    contentString = contentString + '<h3>' +  data.body + ' </h3> </div>';
-	    var infowindow = new google.maps.InfoWindow();
-	    infowindow.setContent(contentString);
-            infowindow.open(map, marker);
-	  activeInfoWindow = infowindow;
-	$('#search').autocomplete('close');
-  });
-$('#search').autocomplete('close');
-});
-
-$(document).ready(function() {
-$("#search").keydown(function(event) {
-// do the extra stuff her
-if (event.keyCode == 13) {
+//function to query api on submit & update google maps with values
+function submitquery(event) {
 	event.preventDefault();
 	console.log( "Handler for .submit() called." );
-	var query = $(this).val();
+	var query = $(document.getElementById('search')).val();
 	$.get("doctorsearch", {'query': query})
-	  .done(function(data){
-	    setTimeout(function() {$('#search').autocomplete('close');}, 1000);
-	    var latlng = new google.maps.LatLng(data.latitude, data.longitude);
-	    if (activeInfoWindow) { activeInfoWindow.close();}
-	    marker.setPosition(latlng);
-	    map.panTo(latlng);
-	    map.setZoom(13);
-	    var contentString = '<div class="info-window"> <h2>' + data.title + '</h2>';
-	    contentString = contentString + '<h3>' +  data.body + ' </h3> </div>';
-	    var infowindow = new google.maps.InfoWindow();
-	    infowindow.setContent(contentString);
-            infowindow.open(map, marker);
-		  activeInfoWindow = infowindow;
-	  });
-	    $('#search').autocomplete('close');
+		.done(function(data){
+			var latlng = new google.maps.LatLng(data.latitude, data.longitude);
+			if (activeInfoWindow) { activeInfoWindow.close();}
+			marker.setPosition(latlng);
+			map.panTo(latlng);
+			map.setZoom(13);
+			var contentString = '<div class="info-window"> <h2>' + data.title + '</h2>';
+			contentString = contentString + '<h4>' +  data.body + ' </h4> </div>';
+			var infowindow = new google.maps.InfoWindow();
+			infowindow.setContent(contentString);
+			infowindow.open(map, marker);
+			activeInfoWindow = infowindow;
+		});
+	$('#search').autocomplete('disable');
+	$('#search').autocomplete('close');
 }
-});
-});
+
+//functions to use when document is ready
 $(document).ready(function() {
-  $("#search").autocomplete({
-    autofocus: true,
-    source: "get_doctors",
-    minLength: 2,
-  });
+
+	// On clicking search button, submit query
+	$("#searchbutton").click(function(event) {
+		submitquery(event);
+	});
+
+	// On hitting enter, submit query, otherwise enable autocomplete
+	$("#search").keydown(function(event) {
+		if (event.keyCode == 13) {
+			submitquery(event);
+		} else {
+			$('#search').autocomplete('enable');
+		}
+	});
+
+	// Attach jquery autocomplete to search input div
+	$("#search").autocomplete({
+		source: "get_doctors",
+		minLength: 2,
+		autoFocus: true,
+		select: function( event, ui ) {
+			event.preventDefault();
+			$( "#search" ).val( ui.item.value );
+			submitquery(event);
+		},
+	});
+
 });
+
+
+// The latitude and longitude of your business / place
+var position = [40.850033, -97.6500523];
+var activeInfoWindow;
+
+function initMap() {
+
+    var latLng = new google.maps.LatLng(position[0], position[1]);
+
+    var mapOptions = {
+        zoom: 5, // initialize zoom level - the max value is 21
+        streetViewControl: false, // hide the yellow Street View pegman
+        scaleControl: true, // allow users to zoom the Google Map
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: latLng
+    };
+
+    map = new google.maps.Map(document.getElementById('googlemaps'),
+        mapOptions);
+
+    // Show the default red marker at the location
+    marker = new google.maps.Marker({
+        position: (null, null),
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', showGoogleMaps);
 
--- physician_maps/core/views.py
+++ physician_maps/core/views.py
@@ -9,7 +9,7 @@ def mapsearch(request):
 def doctorsearch(request):
     query = request.GET.get('query')
     #this view is designed to serve as an api to grab longitude and latitude from a doctor model id
-    doctors = Doctor.objects.filter(name__istartswith= query)
+    doctors = Doctor.objects.filter(name__istartswith= query).order_by('pk')
     if len(doctors) > 0:
         chosen_doc = doctors[0]
         lat, lng = chosen_doc.get_coordinates()
@@ -25,7 +25,7 @@ import json
 def get_doctors(request):
     if request.is_ajax():
         q = request.GET.get('term', '')
-        docs = Doctor.objects.filter(name__istartswith= q )[:10]
+        docs = Doctor.objects.filter(name__istartswith= q ).order_by('pk')[:10]
         results = []
         for doc in docs:
             doc_json = {}
